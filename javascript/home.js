37c7a53a92d9f63c6443ddccccf8b121e00fab4d
Jeffrey Tom <Jeffrey.tom90@gmail.com>
2015-11-10 20:08:04 -0500

    added window effect

---
+++ javascript/home.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	// $(".window").hide(500);
+	// $(".window").fadeIn(3000);
+	// $(".window").fadeOut(10000);
+	$(".window").hide().fadeIn(3000).fadeOut(10000);
+
+});
